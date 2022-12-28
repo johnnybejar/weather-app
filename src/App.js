4869f5425737d1ae44dfad0ebac38e639a3e66c8
johnny <jbejar2001@yahoo.com>
2022-12-28 02:31:29 -0500

    Search query implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import React, { useState } from 'react';
 
 function App() {
-  return null
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const API = {
+    key: '9f5e8b054a638a95bb11203257eba88c',
+    base: 'https://api.openweathermap.org/data/2.5/'
+  }
+
+  const searchWeatherInfo = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${API.base}weather?q=${query}&units=imperial&appid=${API.key}`).then(
+        res => res.json().then(
+          result => {
+            setWeather(result);
+            setQuery('');
+            console.log(result);
+          }
+        )
+      )
+    }
+  }
+
+  return (
+    <div>
+      <input
+      type="text"
+      placeholder='Search'
+      onChange={e => setQuery(e.target.value)}
+      value={query}
+      onKeyUp={searchWeatherInfo}
+      />
+      <div
+      className='current-weather'
+
+      />
+    </div>
+  )
 }
 
 export default App;
-``
